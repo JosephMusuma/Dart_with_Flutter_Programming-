08a17cbaa879037a6f2f6ce752b23de1696d2f48
JosephMusuma <145932102+JosephMusuma@users.noreply.github.com>
2024-03-13 17:08:12 +0300

    Add files via upload

---
+++ Data_Types_in_Dart_Technical_Assignment.dart
@@ -0,0 +1,21 @@
+void main() {
+  // Integer data type (int) is used to store whole numbers
+  int age = 23;
+  print("I am $age years old");
+
+  // Double data type(double) is used to store floating point numbers, that is decimals 
+  double pi = 3.142;
+  print("The value of pi is $pi.");
+
+  // String data type (String) is used to store a series of characters 
+  String car = "Lamborghini";
+  print("My favorite car is $car.");
+
+  // List (List) is used to store an ordered collection of items
+  List<String> friends = ["Brian", "Jonte", "Bright"];
+  print("My best friends are $friends");
+
+  // Map (Map) is used to store key-value pairs
+  Map<String, String> Country = {"Kenya": "Nairobi", "Uganda": "Kampala"};
+  print("The Capital City of Uganda is ${Country["Uganda"]}"); // Access value using key
+}
